83ce0427a4b938098b2dd163f06647f2495406bb
himanshishah-2910 <himanshishah2910@gmail.com>
2026-01-22 19:19:55 +0530

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,15 @@
-import OpenAI from "openai";
+export default async function handler(req, res) {
+  // CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+
+  // ... rest of your chat.js code
+}import OpenAI from "openai";
 
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
